7ea0cdc18088e1e99490ee5de18f6eefeec263d1
mag5323 <mag5323@gmail.com>
2015-01-19 14:01:06 +0800

    fix quote to single quote

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 function ajax(url, callback) {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
+  xhr.open('GET', url, true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
       callback.apply(xhr);
