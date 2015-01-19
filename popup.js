0a9d80a15a2d538a3bf5c11d69b19b02d3f5381c
mag5323 <mag5323@gmail.com>
2015-01-19 13:56:09 +0800

    get currency detail

--- popup.js
+++ popup.js
@@ -14,6 +14,29 @@ ajax('http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm', function() {
     if (p.indexOf('DownloadCsv') > 0) {
       var datePosition = p.indexOf('date');
       var time = p.substr(datePosition + 5, 19);
+
+      ajax('http://rate.bot.com.tw/Pages/UIP003/Download.ashx?lang=zh-TW&fileType=1&date=' + time, function() {
+        var currencies = {};
+        var rates = this.responseText.split('\n');
+        var len = rates.length;
+        var i;
+
+        for (i = 1; i < len - 1; i++) {
+          var row = rates[i].split(',');
+
+          row = row.map(function(column) {
+            return column.trim();
+          });
+
+          var currency = {};
+          currency.cashBuy = row[2];
+          currency.cashSale = row[12];
+          currency.spotBuy = row[3];
+          currency.spotSale = row[13];
+
+          currencies[row[0]] = currency;
+        };
+      });
     }
   });
 });
