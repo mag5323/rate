3bd44188752e842f5664bfc8599e39e7c1ad0cb1
mag5323 <mag5323@gmail.com>
2015-01-19 13:43:09 +0800

    add XMLHttpRequest to popup.js

---
+++ popup.js
@@ -0,0 +1,10 @@
+function ajax(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      callback.apply(xhr);
+    }
+  }
+  xhr.send();
+}
